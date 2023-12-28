6b9664cd299e063761b1e1b4f73874bd7bcc5c3d
PeroumalPajany <83771923+PeroumalPajany@users.noreply.github.com>
2023-12-28 12:03:40 -0500

    Add files via upload

--- TIM8121_5_OpenMP/BMPUtility.cpp
+++ TIM8121_5_OpenMP/BMPUtility.cpp
@@ -7,7 +7,7 @@
 
 
 int invertColorsInFile(std::string inFilePath, std::string outFilePath, int thrds) {
-	// Read the BMP file into memory
+	// Read the BMP file into memory (RAM) as per the assignment requirement 
 	std::ifstream inputFile(inFilePath, std::ios::binary | std::ios::ate);
 	if (!inputFile.is_open()) {
 		std::cerr << "ERROR: Open input file." << std::endl;
@@ -16,17 +16,17 @@ int invertColorsInFile(std::string inFilePath, std::string outFilePath, int thrd
 
 	std::streamsize fileSize = inputFile.tellg();
 	inputFile.seekg(0, std::ios::beg);
-	std::vector<char> imageData(fileSize);
-	if (!inputFile.read(imageData.data(), fileSize)) {
+	std::vector<char> imageBMPData(fileSize);
+	if (!inputFile.read(imageBMPData.data(), fileSize)) {
 		std::cerr << "ERROR: Read input file image." << std::endl;
 		return 1;
 	}
 	inputFile.close();
 
-	int width = *(reinterpret_cast<int*>(&imageData[18]));  // Pos: 18 width
-	int height = *(reinterpret_cast<int*>(&imageData[22])); // Pos: 22 height
+	int width = *(reinterpret_cast<int*>(&imageBMPData[18]));  // Pos: 18 width
+	int height = *(reinterpret_cast<int*>(&imageBMPData[22])); // Pos: 22 height
 	// Invert the colors using OpenMP
-	invertColors(imageData.data() + 54, width, height, thrds); // Pixel data starts from Pos: 54
+	invertColors(imageBMPData.data() + 54, width, height, thrds); // Pixel data starts from Pos: 54
 
 	// Write the inverted image to the output file
 	std::ofstream outputFile(outFilePath, std::ios::binary);
@@ -36,7 +36,7 @@ int invertColorsInFile(std::string inFilePath, std::string outFilePath, int thrd
 	}
 
 	// Write the BMP header with the inverted color image of the original image 
-	outputFile.write(imageData.data(), fileSize);
+	outputFile.write(imageBMPData.data(), fileSize);
 	outputFile.close();
 
 	return 0;
@@ -44,18 +44,17 @@ int invertColorsInFile(std::string inFilePath, std::string outFilePath, int thrd
 }
 
 
-void invertColors(char* imageData, int width, int height, int thrds) {
+void invertColors(char* imageBMPData, int width, int height, int thrds) {
 
 #pragma omp parallel for num_threads(thrds) collapse(2)
 	for (int i = 0; i < height; ++i) {
 		for (int j = 0; j < width; ++j) {
 			int index = 3 * (i * width + j); // Here the each pixel has 3 bytes (RGB)
-			// Invert each byte using the (~) operator
 #pragma omp critical
-			{
-				imageData[index] = ~imageData[index];         // RBG-Red Spectrum
-				imageData[index + 1] = ~imageData[index + 1]; // RBG-Blue Spectrum
-				imageData[index + 2] = ~imageData[index + 2]; // RBG-Green Spectrum
+			{  //Use !~ comparsion as per the assignment requirement
+				imageBMPData[index]		= ~imageBMPData[index];     // RBG-Red Spectrum 
+				imageBMPData[index + 1] = ~imageBMPData[index + 1]; // RBG-Blue Spectrum
+				imageBMPData[index + 2] = ~imageBMPData[index + 2]; // RBG-Green Spectrum
 			}
 		}
 	}
@@ -73,36 +72,36 @@ void bmpToBinary(std::string filePath, std::string outputFileName) {
 		return;
 	}
 
-	// Read the BMP header (54 bytes)
+	// Read the BMP header which is the 54 bytes
 	char bmpHeader[54];
 	file.read(bmpHeader, 54);
 
-	// Calculate the size of the image data (assuming 24 bits per pixel)
+	// Calculate the size of the image data using 24 bits per pixel
 	int dataSize = *(int*)&bmpHeader[18] * *(int*)&bmpHeader[22] * 3;
 
 	// Read the content of the BMP file into a character array
-	char* imageData = new char[dataSize];
-	file.read(imageData, dataSize);
+	char* imageBMPData = new char[dataSize];
+	file.read(imageBMPData, dataSize);
 	file.close();
 
-	// Create a vector to store the binary data
-	std::vector<std::bitset<8>> binaryData;
+	// Use vector processing to store the binary bitmap data
+	std::vector<std::bitset<8>> binaryBMPData;
 
 	// Convert each byte to a binary array and append it to the vector
 	for (int i = 0; i < dataSize; ++i) {
-		binaryData.push_back(std::bitset<8>(imageData[i]));
+		binaryBMPData.push_back(std::bitset<8>(imageBMPData[i]));
 	}
-	delete[] imageData;
+	delete[] imageBMPData;
 
-	// Create a text file to write the binary data (0s and 1s)
+	// Create a text file to write the binary bitmap data (0s and 1s)
 	std::ofstream outputFile(outputFileName, std::ios::out);
 	if (!outputFile.is_open()) {
 		std::cerr << "ERROR: Open output file!" << std::endl;
 		return;
 	}
-	// Write the binary data to the text file
-	for (int i = 0; i < binaryData.size(); ++i) {
-		outputFile << binaryData[i] << " ";
+	// Write the binary bitmap data (0s and 1s) to the text file
+	for (int i = 0; i < binaryBMPData.size(); ++i) {
+		outputFile << binaryBMPData[i] << " ";
 	}
 
 	outputFile.close();
